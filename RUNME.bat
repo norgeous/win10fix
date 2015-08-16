6d7e7eebdc1e486287059c01d08c1c508abf5b1a
norgeous <daniel@danielharte.co.uk>
2015-08-16 03:56:00 +0100

    adjust some variable names

--- RUNME.bat
+++ RUNME.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%wizard\wizard.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
+SET dirpath=%~dp0
+SET ps1script=%dirpath%wizard\wizard.ps1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%ps1script%""' -Verb RunAs}";
--- wizard/ps1.bat
+++ wizard/ps1.bat
@@ -1,2 +1,3 @@
 @ECHO OFF
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass' -Verb RunAs}";
+#PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
