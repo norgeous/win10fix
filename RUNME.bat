e56a8beefcbca8b147d38e7dbefbc390ace54d06
norgeous <daniel@danielharte.co.uk>
2015-08-16 18:04:15 +0100

    rename file

--- RUNME.bat
+++ RUNME.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SET dirpath=%~dp0
-SET bat2ps1=%dirpath%wizard\bat2ps1.bat
+SET ps1exec=%dirpath%wizard\ps1exec.bat
 SET ps1script=%dirpath%wizard\wizard.ps1
-call %bat2ps1% %ps1script%
+call %ps1exec% %ps1script%
