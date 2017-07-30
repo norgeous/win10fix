75dd10a139d9dd1c9edc39da199bf3ce9780306d
Daniel Harte <daniel@harte.me>
2017-07-30 20:01:26 +0100

    Tidy

--- app/md2gui.ps1
+++ app/md2gui.ps1
@@ -193,18 +193,14 @@ function Initialize-Shortcutsbat($Local:configlocation) {
             # code blocked until form returns
             $Local:num_commands = @($Script:commandstorun).Length
             Write-Color -Gray " gui returned $Local:num_commands command(s)..."
-            #for ($Local:i=0; $Local:i -lt $Script:commandstorun.length; $Local:i++) {
             $Local:counter = 0
             Foreach ($Local:command in $Script:commandstorun)
             {
-                #$command = $Script:commandstorun[$Local:i]
                 $Local:label = $Script:commandlabels[$Local:counter]
-                Write-Color -DarkCyan " md2gui.ps1> " -Cyan "$Local:shortcutsfilename> " -Yellow "$Local:label> " -Cyan "$Local:command"
+                #Write-Color -DarkCyan " md2gui.ps1> " -Cyan "$Local:shortcutsfilename> " -Yellow "$Local:label> " -Cyan "$Local:command"
                 iex $Local:command
-                Write-Color -DarkCyan " md2gui.ps1> " -Cyan "$Local:shortcutsfilename> " -Yellow "$Local:label> " -Gray "exitcode: $LASTEXITCODE"
-                If ($LASTEXITCODE) {
-                  Exit $LASTEXITCODE
-                }
+                Write-Color -DarkCyan " md2gui.ps1> " -Cyan "$Local:shortcutsfilename> " -Yellow "$Local:label> " -Cyan "$Local:command " -Gray "exitcode: $LASTEXITCODE"
+                If ($LASTEXITCODE) {Exit $LASTEXITCODE}
                 $Local:counter++
             }
 
