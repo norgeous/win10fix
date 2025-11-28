551cc766ae322c29435ccb40a9da779e826a7cc7
Daniel Harte <norgeous@users.noreply.github.com>
2025-11-28 14:12:20 +0000

    cup -> choco upgrade

--- app/gui/chocolatey/cupally/cupally.bat
+++ app/gui/chocolatey/cupally/cupally.bat
@@ -1,3 +1,3 @@
 @ECHO off
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""Write-Host; cup all -y; Write-Host; Start-Sleep -s 5""' -Verb RunAs}";
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""Write-Host; choco upgrade all -y; Write-Host; Start-Sleep -s 5""' -Verb RunAs}";
